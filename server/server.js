e24142f6f99d385911af8395a179fe790eb08d24
Daniel <dprevoznik@gmail.com>
2020-04-16 11:37:27 -0400

    Changed whitelist configuration for cors.

--- server/server.js
+++ server/server.js
@@ -5,18 +5,19 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3017;
 
-const whitelist = ["http://localhost:3000", "http://localhost:3017", "https://loader.io"]
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const whitelist = ["http://localhost:3000", "http://localhost:3017", "https://loader.io"]
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.json());
 
